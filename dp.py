b57b57943967b64b9d7fbbacae2a6912eb3c750c
taisteluvalmis <144608802+taisteluvalmis@users.noreply.github.com>
2023-09-24 19:35:33 +0300

    Create dp.py

---
+++ dp.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from os import getenv
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
+db = SQLAlchemy(app)
